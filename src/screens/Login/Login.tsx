c1042d989e5faad0e0c3c6962c0f8e5356854ce9
= <=>
2022-09-14 15:56:36 -0500

    cookie added

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -183,6 +183,8 @@ export const Login = () => {
         if (r.status === 200) {
           localStorage.setItem("IdUsuario", r.data.IdUsuario);
           localStorage.setItem("jwtToken", r.data.token);
+          document.cookie = "jwt="+r.data.token;
+          
           setAppsList(r.data.AppIds);
           openAppModal(
             "success",
