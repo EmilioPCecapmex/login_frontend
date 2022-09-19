d726c42199e3289b638e87a43bdc1f1dfdef1603
= <=>
2022-09-19 15:23:55 -0500

    jwt get url

--- src/components/appsModal.tsx
+++ src/components/appsModal.tsx
@@ -130,7 +130,7 @@ export default function AppsModal({
                 <Button
                   variant="outlined"
                   key={item.IdApp}
-                  onClick={() => window.open(item.Path)}
+                  onClick={() => window.open(item.Path+"?jwt="+ localStorage.getItem("jwtToken"))}
                   sx={{
                     borderColor: "#62787B",
                     width: "30%",
