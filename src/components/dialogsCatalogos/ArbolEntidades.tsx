c139e575210d4084f5818f91283f279b88934b82
PedroPardoG <112125266+PedroPardoG@users.noreply.github.com>
2024-02-22 14:52:29 -0600

    arbol de entidadeds

--- src/components/dialogsCatalogos/ArbolEntidades.tsx
+++ src/components/dialogsCatalogos/ArbolEntidades.tsx
@@ -11,6 +11,7 @@ import ButtonsAdd from "../../screens/Componentes/ButtonsAdd";
 import { useState } from "react";
 import { Create } from "./Create";
 import { newCatalogo } from "../../screens/Catalogos/Catalogos";
+import SliderProgress from "../../screens/Componentes/SliderProgress";
 
 export const ArbolEntidades = ({
   open,
@@ -22,56 +23,62 @@ export const ArbolEntidades = ({
   const [add, setAdd] = useState(false);
 
   return (
-  <>
-  <Dialog
-      open={open}
-      onClose={() => {
-        setOpen(false);
-      }}
-      fullWidth
-      maxWidth={"xl"}
-    >
-
-      {add?<Create
-        open={add}
-        setOpen={setAdd}
-        catalogo={"Entidades"}
-        data={newCatalogo}
-      />:
-      <>
-      <DialogTitle>
-        <Grid sx={{display:"flex", width:"100%", justifyContent:"space-between"}}>
-          Entidades:
-          <ButtonsAdd
-            title={"Agregar Entidad"}
-            handleOpen={() => {
-              setAdd(true);
-            }}
-            agregar={true}
-          />
-        </Grid>
-      </DialogTitle><DialogContent
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
+    <>
+      <Dialog
+        open={open}
+        onClose={() => {
+          setOpen(false);
         }}
+        fullWidth
+        maxWidth={"xl"}
       >
-        <Grid sx={{ width: "100%" }}>
-          <AcordionesGenericos id="" tabla="EntidadesMatrices" />
-        </Grid>
-
-       
-      </DialogContent><DialogActions>
-       
-        <Button className="cancelar" onClick={() => setOpen(false)}>
-          Cerrar
-        </Button>
-      </DialogActions>
-      </>
-      }
-    </Dialog>
-  </>
-    
+        {add ? (
+          <Create
+            open={add}
+            setOpen={setAdd}
+            catalogo={"Entidades"}
+            data={newCatalogo}
+          />
+        ) : (
+          <>
+            <DialogTitle>
+              <Grid
+                sx={{
+                  display: "flex",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                Entidades:
+                <ButtonsAdd
+                  title={"Agregar Entidad"}
+                  handleOpen={() => {
+                    setAdd(true);
+                  }}
+                  agregar={true}
+                />
+              </Grid>
+            </DialogTitle>
+            <DialogContent
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Grid sx={{ width: "100%" }}>
+                
+                <AcordionesGenericos id="" tabla="EntidadesMatrices" />
+              </Grid>
+            </DialogContent>
+            <DialogActions>
+              <Button className="cancelar" onClick={() => setOpen(false)}>
+                Cerrar
+              </Button>
+            </DialogActions>
+          </>
+        )}
+      </Dialog>
+    </>
   );
 };
