7e1f9e433caa130db063dc296985b82bf852c8f9
PedroPardoG <prpardo@cecapmex.com>
2024-10-23 10:46:37 -0600

    estandarizacion de pantalla y configuracion de descarga de archivos

--- src/screens/ActividadUsuarios/ActividadUsuarios.tsx
+++ src/screens/ActividadUsuarios/ActividadUsuarios.tsx
@@ -1,46 +1,42 @@
-import React, { useState, useEffect } from "react";
 import AppsIcon from "@mui/icons-material/Apps";
+import SearchIcon from '@mui/icons-material/Search';
 import {
+  Button,
   CardContent,
   Grid,
-  Tooltip,
-  Typography,
-  TextField,
-  Button,
   Hidden,
+  Tooltip,
+  Typography
 } from "@mui/material";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import dayjs, { Dayjs } from "dayjs"; // Importar dayjs
-import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
-import MUIXDataGrid from "../../components/dataGridGenerico/MUIXDataGrid";
 import { Header } from "../../components/header";
 import { getActividadUsuarios } from "../../services/hisotoricoService";
-import SearchIcon from '@mui/icons-material/Search';
+import MUIXDataGridGeneral from "../../components/dataGridGenerico/MUIXDataGrid";
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
 
 export const ActividadUsuarios = () => {
   const camposCsv = [
-    "Nombre",
-    "Descripcion",
-    "Path",
     "NombreUsuario",
-    "estatusLabel",
+    "App",
+    "Movimiento",
+    "Fecha",
+    "Hora",
   ];
-
-  const navigate = useNavigate();
   const [rows, setRows] = useState([]);
   const [selectedDate, setSelectedDate] = useState<Dayjs | null>(dayjs()); // Inicializar con el día de hoy
-  const [selectedDateEnd, setSelectedDateEnd] = useState<Dayjs | null>(dayjs()); 
+  const [selectedDateEnd, setSelectedDateEnd] = useState<Dayjs | null>(dayjs());
   // Estado de la fecha
 
   const columns = [
     {
       field: "NombreUsuario",
       headerName: "Nombre Usuario",
-      width:350,
+      width: 350,
       hideable: false,
       headerAlign: "center",
       align: "left",
@@ -48,28 +44,28 @@ export const ActividadUsuarios = () => {
     {
       field: "App",
       headerName: "App",
-      width:750,
+      width: 750,
       headerAlign: "center",
       align: "left",
     },
     {
       field: "Accion",
-      headerName: "Accion",
-      width:300,
+      headerName: "Movimiento",
+      width: 300,
       headerAlign: "center",
       align: "center",
     },
     {
       field: "Fecha",
       headerName: "Fecha",
-      width:200,
+      width: 200,
       headerAlign: "center",
       align: "center",
     },
     {
       field: "Hora",
       headerName: "Hora",
-      width:200,
+      width: 200,
       headerAlign: "center",
       align: "center",
     },
@@ -78,7 +74,7 @@ export const ActividadUsuarios = () => {
   // Aquí es el consumo del endpoint para obtener el listado de apps de la base de datos
 
   const fetchActividadUsuarios = async () => {
-    const data = await getActividadUsuarios(selectedDate,selectedDateEnd); // Llamar a la función asíncrona
+    const data = await getActividadUsuarios(selectedDate, selectedDateEnd); // Llamar a la función asíncrona
     setRows(data); // Actualizar el estado con los datos obtenidos
   };
   useEffect(() => {
@@ -101,51 +97,50 @@ export const ActividadUsuarios = () => {
       >
         <Grid container sx={{ height: "84vh", width: "100vw" }}>
           <Grid
-          item container
+            item container
             sx={{
               display: "flex",
-              justifyContent: "space-around",
+              justifyContent: "space-between",
               alignItems: "center",
-              width: "100%",
-             
+              width: "100%"
             }}
           > <Hidden smDown>
-            <Grid
-              item
-              sx={{
-                display: "flex",
-                
-                alignItems: "center",
-              }}
-            >
-              <Tooltip title="Menu actual: Actividad de Usuarios">
-                <CardContent>
-                  <AppsIcon
-                    sx={{ color: "#AF8C55", fontSize: [30, 30, 30, 40, 40] }}
-                  />
-                </CardContent>
-              </Tooltip>
-
-              <Typography
-                fontFamily={"'Montserrat', sans-serif"}
+              <Grid
+                item
                 sx={{
-                  whiteSpace: "nowrap",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  textAlign: "center",
-                  fontSize: [30, 30, 30, 30, 40], // Tamaños de fuente para diferentes breakpoints
-                  color: "#AF8C55",
+                  display: "flex",
+
+                  alignItems: "center",
                 }}
               >
-                Actividad de Usuarios
-              </Typography>
-            </Grid>
+                <Tooltip title="Menu actual: Actividad de Usuarios">
+                  <CardContent>
+                    <SupervisedUserCircleIcon
+                      sx={{ color: "#AF8C55", fontSize: [30, 30, 30, 40, 40] }}
+                    />
+                  </CardContent>
+                </Tooltip>
+
+                <Typography
+                  fontFamily={"'Montserrat', sans-serif"}
+                  sx={{
+                    whiteSpace: "nowrap",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    textAlign: "center",
+                    fontSize: [30, 30, 30, 30, 40], // Tamaños de fuente para diferentes breakpoints
+                    color: "#AF8C55",
+                  }}
+                >
+                  Actividad de Usuarios
+                </Typography>
+              </Grid>
             </Hidden>
             {/* Componente de selector de fecha */}
             <Grid item xs={12}
-                lg={5}
-                md={5}
-                sm={12} container sx={{ display: "flex", justifyContent: "space-around"}}>
+              lg={5}
+              md={5}
+              sm={12} container sx={{ display: "flex", justifyContent: "space-around" }}>
               <Grid
                 xs={5}
                 lg={3}
@@ -157,7 +152,7 @@ export const ActividadUsuarios = () => {
                     value={selectedDate}
                     onChange={(newValue) => setSelectedDate(newValue)} // Actualizar el estado al seleccionar la fecha
                     slotProps={{ textField: { fullWidth: true } }} // Utiliza slotProps para personalizar TextField
-                  />
+                    />
                 </LocalizationProvider>
               </Grid>
               <Grid
@@ -170,6 +165,7 @@ export const ActividadUsuarios = () => {
                     label="Selecciona la fecha final"
                     value={selectedDateEnd}
                     onChange={(newValue) => setSelectedDateEnd(newValue)} // Actualizar el estado al seleccionar la fecha
+                    minDate={selectedDate} // Restringir la fecha mínima a la fecha inicial
                     slotProps={{ textField: { fullWidth: true } }} // Utiliza slotProps para personalizar TextField
                   />
                 </LocalizationProvider>
@@ -178,40 +174,39 @@ export const ActividadUsuarios = () => {
                 xs={11}
                 lg={2}
                 md={2}
-                sm={11} sx={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+                sm={11} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                 <Button
-                      className="aceptar"
-                      variant="text"
-                      onClick={() => {
-                        fetchActividadUsuarios();
-                      }}
-                      sx={{
-                        fontFamily: "MontserratBold",
-                        backgroundColor: "#DFA94F",
-                        color: "#000001",
-                        boxShadow: 4,
-                        padding: ["1vw", "1vw", "1vw", ".8vw", ".8vw"],
-                        width: ["100%", "90%", "auto", "auto", "auto"],
-                        mt:["1vh","0vh","0vh","0vh","0vh"]
-                      }}
-                      startIcon={<SearchIcon />}
-                    >
-                      <Typography
-                        sx={{
-                          fontSize: [".7rem", ".7rem", ".7rem", "1rem", "1rem"],
-                        }}
-                      >
-                        Buscar
-                      </Typography>
-                    </Button>
+                  className="aceptar"
+                  variant="text"
+                  onClick={() => {
+                    fetchActividadUsuarios();
+                  }}
+                  sx={{
+                    fontFamily: "MontserratBold",
+                    backgroundColor: "#DFA94F",
+                    color: "#000001",
+                    boxShadow: 4,
+                    padding: ["1vw", "1vw", "1vw", ".8vw", ".8vw"],
+                    width: ["100%", "90%", "auto", "auto", "auto"],
+                    mt: ["1vh", "0vh", "0vh", "0vh", "0vh"]
+                  }}
+                  startIcon={<SearchIcon />}
+                >
+                  <Typography
+                    sx={{
+                      fontSize: [".7rem", ".7rem", ".7rem", "1rem", "1rem"],
+                    }}
+                  >
+                    Buscar
+                  </Typography>
+                </Button>
               </Grid>
 
             </Grid>
           </Grid>
 
-          <Grid item sx={{ width: "100vw", height: "71vh" }}>
-            <MUIXDataGrid
-              id={(row: any) => row.Id}
+          <Grid item sx={{ width: "100vw", height: "79vh" }}>
+            <MUIXDataGridGeneral
               columns={columns}
               rows={rows}
               camposCsv={camposCsv}
